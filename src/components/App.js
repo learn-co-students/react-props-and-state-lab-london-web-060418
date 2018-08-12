d19dbfebad4650eecca4db93a760f2ab5ee5ba90
Allegra <aukseallegra@gmail.com>
2018-08-12 19:24:30 +0100

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,36 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = ({ target: {value} }) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: value
+      }
+    })
+  }
+
+  fetchPets = () => {
+    let endpoint = '/api/pets';
+
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets }));
+  };
+
+  onAdoptPet = (id) => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === id ? {...pet, isAdopted: true} : pet
+    })
+    this.setState({
+      pets
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +54,11 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType}
+                onFindPetsClick={this.fetchPets}  />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
@@ -37,3 +68,15 @@ class App extends React.Component {
 }
 
 export default App
+
+
+// const allPets = [
+//   {
+//     id: '5c142d9e-ea45-4231-8146-cccf71c704c0',
+//     type: 'dog',
+//     gender: 'male',
+//     age: 4,
+//     weight: 1,
+//     name: 'Trident',
+//     isAdopted: false,
+//   },
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -3,23 +3,29 @@ import React from 'react'
 class Pet extends React.Component {
   render() {
     return (
-      <div className="card">
+      <div className="card"
+        style={{border: "1px solid #000", margin: "10px", textAlign: "center"}}>
         <div className="content">
-          <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
-          </a>
+          <h3 className="header">
+            {this.props.pet.gender === 'female' ? '♀' : '♂'}
+            {this.props.pet.name}
+          </h3>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">...is a {this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight} kg</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.props.pet.isAdopted ? (
+            <button className="ui disabled button">Already adopted</button>
+          ) : (
+            <button onClick={() => this.props.onAdoptPet(this.props.pet.id)} className="ui primary button">
+              Adopt pet
+            </button>
+          )}
         </div>
       </div>
     )
@@ -27,3 +33,16 @@ class Pet extends React.Component {
 }
 
 export default Pet
+
+//  < Pet pet={pet} key={pet.id} onAdoptPet={this.props.onAdoptPet} />
+
+
+// {
+//   id: '5c142d9e-ea45-4231-8146-cccf71c704c0',
+//   type: 'dog',
+//   gender: 'male',
+//   age: 4,
+//   weight: 1,
+//   name: 'Trident',
+//   isAdopted: false,
+// },
